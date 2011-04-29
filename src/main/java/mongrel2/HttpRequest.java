992b1111eca2619b22303d383214942480b1a811
kwo <karl@ostendorf.com>
2011-04-29 16:53:43 +0200

    removed HttpBaseObject

--- src/main/java/mongrel2/HttpRequest.java
+++ src/main/java/mongrel2/HttpRequest.java
@@ -1,20 +1,26 @@
 package mongrel2;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.TimeZone;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class HttpRequest extends BaseHttpObject {
+public class HttpRequest {
 
 	public static final String ATTR_REQUEST_ID = "request-id";
 	public static final String ATTR_SENDER_ADDR = "sender-addr";
 
+	private static final String H_CONTENT_LENGTH = "Content-Length";
+	private static final String H_CONTENT_TYPE = "Content-Type";
 	private static final String H_METHOD = "METHOD";
 	private static final String H_PROTOCOL = "VERSION";
+
 	private static final String H_QUERY_STRING = "QUERY";
 	private static final String H_SERVLET_PATH = "PATTERN";
 	private static final String H_URI = "URI";
@@ -79,12 +85,25 @@ public class HttpRequest extends BaseHttpObject {
 		return -1;
 	}
 
+	protected byte[] content = new byte[0];
+
 	private final Map<String, Object> attributes;
 
 	private String cachedRequestURI = null;
 
+	private final SimpleDateFormat df;
+
+	private final Map<String, String[]> headers;
+
 	public HttpRequest() {
+		this.headers = new HashMap<String, String[]>();
+		this.df = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss z");
+		this.df.setTimeZone(TimeZone.getTimeZone("GMT"));
 		this.attributes = new HashMap<String, Object>();
+	}
+
+	public boolean containsHeader(final String name) {
+		return this.headers.containsKey(name);
 	}
 
 	public Object getAttribute(final String name) {
@@ -93,6 +112,56 @@ public class HttpRequest extends BaseHttpObject {
 
 	public Iterable<String> getAttributeNames() {
 		return this.attributes.keySet();
+	}
+
+	public byte[] getContent() {
+		return this.content;
+	}
+
+	public int getContentLength() {
+		return getIntHeader(H_CONTENT_LENGTH);
+	}
+
+	public String getContentType() {
+		return getHeader(H_CONTENT_TYPE);
+	}
+
+	public long getDateHeader(final String name) {
+		if (!containsHeader(name))
+			return -1;
+		try {
+			return this.df.parse(getHeader(name)).getTime();
+		} catch (final ParseException x) {
+			throw new RuntimeException(x);
+		}
+	}
+
+	/**
+	 * Returns the first value for the header. If there are multiple values for
+	 * a header, use {@link #getHeaderValues(String)}
+	 * 
+	 * @param name
+	 * @return
+	 */
+	public String getHeader(final String name) {
+		final String key = name;
+		if (containsHeader(key))
+			return getHeaderValues(key)[0];
+		return null;
+	}
+
+	public Iterable<String> getHeaderNames() {
+		return this.headers.keySet();
+	}
+
+	public String[] getHeaderValues(final String name) {
+		return this.headers.get(name);
+	}
+
+	public int getIntHeader(final String name) {
+		if (!containsHeader(name))
+			return -1;
+		return Integer.parseInt(getHeader(name));
 	}
 
 	public String getMethod() {
@@ -142,12 +211,57 @@ public class HttpRequest extends BaseHttpObject {
 		return getHeader(H_SERVLET_PATH);
 	}
 
-	public void removeAttribute(final String name) {
+	void addDateHeader(final String name, final long value) {
+		addHeader(name, this.df.format(value));
+	}
+
+	void addHeader(final String name, final String value) {
+		if (!containsHeader(name)) {
+			setHeader(name, value);
+		} else {
+			final String[] values = getHeaderValues(name);
+			final String[] newValues = new String[values.length + 1];
+			for (int i = 0; i < values.length; i++)
+				newValues[i] = values[i];
+			newValues[newValues.length - 1] = value;
+			this.headers.put(name, newValues);
+		}
+	}
+
+	void addIntHeader(final String name, final int value) {
+		addHeader(name, Integer.toString(value));
+	}
+
+	void removeAttribute(final String name) {
 		this.attributes.remove(name);
 	}
 
-	public void setAttribute(final String name, final Object value) {
+	void setAttribute(final String name, final Object value) {
 		this.attributes.put(name, value);
+	}
+
+	void setContent(final byte[] content) {
+		this.content = content;
+	}
+
+	void setContentLength(final int size) {
+		setIntHeader(H_CONTENT_LENGTH, size);
+	}
+
+	void setContentType(final String mimetype) {
+		setHeader(H_CONTENT_TYPE, mimetype);
+	}
+
+	void setDateHeader(final String name, final long date) {
+		setHeader(name, this.df.format(date));
+	}
+
+	void setHeader(final String name, final String value) {
+		this.headers.put(name, new String[] { value });
+	}
+
+	void setIntHeader(final String name, final int value) {
+		setHeader(name, Integer.toString(value));
 	}
 
 	void setRequestId(final String requestId) {
--- src/main/java/mongrel2/HttpResponse.java
+++ src/main/java/mongrel2/HttpResponse.java
@@ -1,24 +1,66 @@
 package mongrel2;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TimeZone;
 import java.util.concurrent.TimeUnit;
 
-public class HttpResponse extends BaseHttpObject {
+public class HttpResponse {
 
+	private static final String H_CONTENT_LENGTH = "Content-Length";
+	private static final String H_CONTENT_TYPE = "Content-Type";
+	private static final String H_DATE = "Date";
 	private static final String H_EXPIRES = "Expires";
 	private static final String H_LAST_MODIFIED = "Last-Modified";
-
+	private byte[] content = new byte[0];
+	private final SimpleDateFormat df;
+	private final Map<String, String[]> headers;
 	private int statusCode = 0;
 	private String statusMessage = null;
 
 	public HttpResponse() {
+		this.headers = new HashMap<String, String[]>();
+		this.df = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss z");
+		this.df.setTimeZone(TimeZone.getTimeZone("GMT"));
 	}
 
-	public int getStatus() {
-		return this.statusCode;
+	public void addDateHeader(final String name, final long value) {
+		addHeader(name, this.df.format(value));
 	}
 
-	public String getStatusMessage() {
-		return this.statusMessage;
+	public void addHeader(final String name, final String value) {
+		if (!containsHeader(name)) {
+			setHeader(name, value);
+		} else {
+			final String[] values = getHeaderValues(name);
+			final String[] newValues = new String[values.length + 1];
+			for (int i = 0; i < values.length; i++)
+				newValues[i] = values[i];
+			newValues[newValues.length - 1] = value;
+			this.headers.put(name, newValues);
+		}
+	}
+
+	public void addIntHeader(final String name, final int value) {
+		addHeader(name, Integer.toString(value));
+	}
+
+	public void setContent(final byte[] content) {
+		this.content = content;
+	}
+
+	public void setContentLength(final int size) {
+		setIntHeader(H_CONTENT_LENGTH, size);
+	}
+
+	public void setContentType(final String mimetype) {
+		setHeader(H_CONTENT_TYPE, mimetype);
+	}
+
+	public void setDateHeader(final String name, final long date) {
+		setHeader(name, this.df.format(date));
 	}
 
 	public void setExpires(final int value, final TimeUnit unit) {
@@ -60,6 +102,14 @@ public class HttpResponse extends BaseHttpObject {
 		setDateHeader(H_EXPIRES, date);
 	}
 
+	public void setHeader(final String name, final String value) {
+		this.headers.put(name, new String[] { value });
+	}
+
+	public void setIntHeader(final String name, final int value) {
+		setHeader(name, Integer.toString(value));
+	}
+
 	public void setLastModified(final long date) {
 		setDateHeader(H_LAST_MODIFIED, date);
 	}
@@ -80,6 +130,61 @@ public class HttpResponse extends BaseHttpObject {
 
 	public void setTimestampHeader(final long time) {
 		setDateHeader(H_DATE, time);
+	}
+
+	boolean containsHeader(final String name) {
+		return this.headers.containsKey(name);
+	}
+
+	byte[] getContent() {
+		return this.content;
+	}
+
+	int getContentLength() {
+		return getIntHeader(H_CONTENT_LENGTH);
+	}
+
+	String getContentType() {
+		return getHeader(H_CONTENT_TYPE);
+	}
+
+	long getDateHeader(final String name) {
+		if (!containsHeader(name))
+			return -1;
+		try {
+			return this.df.parse(getHeader(name)).getTime();
+		} catch (final ParseException x) {
+			throw new RuntimeException(x);
+		}
+	}
+
+	String getHeader(final String name) {
+		final String key = name;
+		if (containsHeader(key))
+			return getHeaderValues(key)[0];
+		return null;
+	}
+
+	Iterable<String> getHeaderNames() {
+		return this.headers.keySet();
+	}
+
+	String[] getHeaderValues(final String name) {
+		return this.headers.get(name);
+	}
+
+	int getIntHeader(final String name) {
+		if (!containsHeader(name))
+			return -1;
+		return Integer.parseInt(getHeader(name));
+	}
+
+	int getStatus() {
+		return this.statusCode;
+	}
+
+	String getStatusMessage() {
+		return this.statusMessage;
 	}
 
 }
