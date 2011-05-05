004f096e38ce092f0b2a5df202294fdfc056987f
kwo <karl@ostendorf.com>
2011-05-05 19:00:20 +0200

    code cleanup

--- src/test/java/mongrel2/LiveTest.java
+++ src/test/java/mongrel2/LiveTest.java
@@ -16,112 +16,128 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class LiveTest {
-	
-	
-	// find m2sh in path
-	// create uuid tmp dir in tmp dir
-	// create tmp, run, logs
-	
+
 	private static final String CFG_FILE = "livetest.conf";
 	private static String m2sh = null;
 	private static File workdir = null;
-	
+
 	@BeforeClass
 	public static void setup() throws Exception {
-		
+
 		// find m2sh
 		m2sh = findM2sh();
 		Assert.assertNotNull(m2sh);
-		
+
 		// create workdir
 		workdir = createWorkingDirectory();
 		Assert.assertTrue(workdir.exists());
 		Assert.assertTrue(workdir.isDirectory());
-		
+
 		// shell to m2sh, load configuration
 		loadConfiguration(m2sh, workdir);
 		Assert.assertTrue(new File(workdir, "config.sqlite").exists());
-		
+
 		// TODO: start mongrel2
-		
+
 	}
 
 	@AfterClass
 	public static void teardown() throws Exception {
-		
+
 		// TODO: stop/kill mongrel
-		
+
 		// remove workdir
 		removeWorkingDirectory(workdir);
 		Assert.assertTrue(!workdir.exists());
-		
+
 	}
 
-	@Test
-	public void testMongrel2() throws Exception {
-		//Assert.fail(workdir.getPath());
+	private static File createWorkingDirectory() throws Exception {
+
+		final String wd = "mongrel2-" + UUID.randomUUID().toString();
+		final File tmp1 = File.createTempFile("tmp", null);
+		tmp1.delete();
+
+		final File workdir = new File(tmp1.getParentFile(), wd);
+		final File rundir = new File(workdir, "run");
+		final File tmpdir = new File(workdir, "tmp");
+		final File logdir = new File(workdir, "logs");
+
+		workdir.mkdir();
+		rundir.mkdir();
+		tmpdir.mkdir();
+		logdir.mkdir();
+
+		return workdir;
+
 	}
-	
-	private static void loadConfiguration(String m2sh, File workdir) throws Exception {
-		
-		File livetest = new File(workdir, CFG_FILE);
+
+	private static void extractConfiguration(final File target) throws Exception {
+
+		final OutputStream out = new BufferedOutputStream(new FileOutputStream(target));
+		final InputStream in = LiveTest.class.getResourceAsStream(CFG_FILE);
+
+		int len = 0;
+		final byte[] buf = new byte[256];
+		while ((len = in.read(buf)) > -1)
+			out.write(buf, 0, len);
+		out.close();
+		in.close();
+
+	}
+
+	private static String findM2sh() throws Exception {
+
+		String m2sh = null;
+
+		// build directories for search
+		final List<String> pathdirs = new ArrayList<String>();
+
+		// add search path to directories
+		final String path = System.getenv("PATH");
+		if (path != null)
+			for (final String pathdir : path.split(File.pathSeparator))
+				pathdirs.add(pathdir);
+
+		// add other common locations
+		pathdirs.add("/usr/local/bin");
+
+		// start looking
+		for (final String pathdir : pathdirs) {
+			final File f = new File(pathdir + File.separator + "m2sh");
+			if (f.exists()) {
+				m2sh = f.getPath();
+				break;
+			}
+		}
+
+		return m2sh;
+
+	}
+
+	private static void loadConfiguration(final String m2sh, final File workdir) throws Exception {
+
+		final File livetest = new File(workdir, CFG_FILE);
 		extractConfiguration(livetest);
-		Process p = Runtime.getRuntime().exec(new String[]{ m2sh, "load", "--db", "config.sqlite", "--config", CFG_FILE } , new String[0], workdir);
-		
+		final Process p = Runtime.getRuntime().exec(
+				new String[] { m2sh, "load", "--db", "config.sqlite", "--config", CFG_FILE }, new String[0], workdir);
+
 		while (true) {
 			try {
 				Thread.sleep(500);
 				p.exitValue();
 				break;
-			} catch (IllegalThreadStateException x) {
+			} catch (final IllegalThreadStateException x) {
 				// wait
 			}
 		}
-		
+
 		Assert.assertEquals(0, p.exitValue());
 	}
-	
-	private static void extractConfiguration(File target) throws Exception {
-		
-		OutputStream out = new BufferedOutputStream(new FileOutputStream(target));
-		InputStream in = LiveTest.class.getResourceAsStream(CFG_FILE);
-		
-		int len = 0;
-		byte[] buf = new byte[256];
-		while ((len = in.read(buf)) > -1)
-			out.write(buf, 0, len);
-		out.close();
-		in.close();
-		
-	}
-	
-	private static File createWorkingDirectory() throws Exception {
-		
-		String wd = "mongrel2-" + UUID.randomUUID().toString();
-		File tmp1 = File.createTempFile("tmp", null);
-		tmp1.delete();
 
-		File workdir = new File(tmp1.getParentFile(), wd);
-		File rundir = new File(workdir, "run");
-		File tmpdir = new File(workdir, "tmp");
-		File logdir = new File(workdir, "logs");
-		
-		workdir.mkdir();
-		rundir.mkdir();
-		tmpdir.mkdir();
-		logdir.mkdir();
-		
-		return workdir;
-		
-	}
-	
-	private static void removeWorkingDirectory(File dir) {
-		removeDirectory(dir);
-	}
-
-	private static void removeDirectory(File target) {
-		File[] ff = target.listFiles();
-		for (File f : ff) {
+	private static void removeDirectory(final File target) {
+		final File[] ff = target.listFiles();
+		for (final File f : ff) {
 			if (f.isDirectory()) {
 				removeDirectory(f);
 			} else {
@@ -130,35 +146,14 @@ public class LiveTest {
 		}
 		target.delete();
 	}
-	
-	private static String findM2sh() throws Exception {
-		
-		String m2sh = null;
 
-		// build directories for search
-		List<String> pathdirs = new ArrayList<String>();
-		
-		// add search path to directories
-		String path = System.getenv("PATH");
-		if (path != null)
-			for (String pathdir : path.split(File.pathSeparator))
-				pathdirs.add(pathdir);
-
-		// add other common locations
-		pathdirs.add("/usr/local/bin");
-		
-		// start looking
-		for (String pathdir : pathdirs) {
-			File f = new File(pathdir + File.separator + "m2sh");
-			if (f.exists()) {
-				m2sh = f.getPath();
-				break;
-			}
-		}
-		
-		return m2sh;
-		
+	private static void removeWorkingDirectory(final File dir) {
+		removeDirectory(dir);
 	}
-	
+
+	@Test
+	public void testMongrel2() throws Exception {
+		// Assert.fail(workdir.getPath());
+	}
 
 }
